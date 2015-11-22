85d0f6abba25245d2d9edd26552ae2eeb87f20a4
RensWiebenga <renswiebenga@gmail.com>
2015-11-22 15:34:50 +0100

    new portfolio

---
+++ javascript/portfolio.js
@@ -0,0 +1,58 @@
+var portfolio = {
+	title: 'portfolio',
+	portfoliopieces: [ {
+		id: 1,
+		title: 'E-modulewijzer',
+		image: 'e-modulewijzer.jpg',
+		description: 'E-modulewijzer is a tool which allows the producers of educational tools to estimate how much time is required by the different roles/people in the project',
+		year: '2015',
+		url: 'http://e-modulewijzer.nl',
+		extraimage: ['']
+	},
+	{
+		id: 2,
+		title: 'GameRealm',
+		image: 'gamerealm.jpg',
+		description: 'Gamerealm is a platform created for gamers and indy game developers. Gamerealm functions as a platform for gamers.',
+		year: '2014',
+		url: 'http://gamerealm.eu',
+		extraimage: ['']
+	},
+	{
+		id: 3,
+		title: 'Vascular Risk app (Vaatrisico app)',
+		image: 'vaatrisico_large.png',
+		description: 'Co-creator of the Vascular Risk app. Designed so healthcare providers can easily calculate the risk of cardiovascular disease of their patients.',
+		year: '2014',
+		url: 'https://itunes.apple.com/nl/app/vaatrisico/id571079909?mt=8',
+		extraimage: ['']
+	},
+	{
+		id: 4,
+		title: 'I want one of those memes',
+		image: 'iwantoneofthosememes.jpg',
+		description: 'The webshop \'I want one of those memes\' is a side-project is a business venture created to sell meme-merchandise.',
+		year: '2012',
+		url: 'http://iwantoneofthosememes.com/',
+		extraimage: ['']
+	},
+	{
+		id: 5,
+		title: 'Portfolio for Linda Baars',
+		image: 'lindabaars.jpg',
+		description: 'This search engine optimized website is a portfolio for social worker Linda Baars to optimally further her online presence and career. ',
+		year: '2012',
+		url: 'http://lindabaars.com/',
+		extraimage: ['']
+	},
+	{
+		id: 6,
+		title: 'Sketchwork',
+		image: 'dragon_in_the_night.jpg',
+		description: 'I also do sketchwork to hone my design skills for logo and web design.',
+		year: '2010',
+		url: '',
+		extraimage: ['dragon_in_the_night.jpg', 'machanical_lizard.jpg','android.jpg']
+	}
+	]
+};
